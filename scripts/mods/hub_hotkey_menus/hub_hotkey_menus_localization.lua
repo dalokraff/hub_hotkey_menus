5245cba8514c424ddd1e9779e4ef02f57acd33ad
deluxghost <deluxghost@users.noreply.github.com>
2023-02-28 17:04:36 +0800

    add zh-cn localization

--- scripts/mods/hub_hotkey_menus/hub_hotkey_menus_localization.lua
+++ scripts/mods/hub_hotkey_menus/hub_hotkey_menus_localization.lua
@@ -1,64 +1,82 @@
 return {
   mod_name = {
     en = "Hub Hotkey Menus",
+    ["zh-cn"] = "大厅菜单快捷键",
   },
   mod_description = {
     en = "Open various menus from the hub world with hotkeys.",
+    ["zh-cn"] = "通过快捷键打开哀星号上的各种菜单。",
   },
   -- barber_vendor_background_view
   open_barber_view_key = {
     en = "Barber",
+    ["zh-cn"] = "理发师",
   },
   open_barber_view_key_description = {
     en = "Opens the Barber view.",
+    ["zh-cn"] = "打开理发师兼外科医生界面。",
   },
   -- contracts_background_view
   open_contracts_view_key = {
     en = "Contracts",
+    ["zh-cn"] = "协议",
   },
   open_contracts_view_key_description = {
     en = "Opens the Contracts view.",
+    ["zh-cn"] = "打开梅尔克大人的采购店界面。",
   },
   -- crafting_view
   open_crafting_view_key = {
     en = "Crafting",
+    ["zh-cn"] = "锻造",
   },
   open_crafting_view_key_description = {
     en = "Opens the Crafting view.",
+    ["zh-cn"] = "打开 0-7-7 海德昂界面。",
   },
   -- credits_vendor_background_view
   open_credits_vendor_view_key = {
     en = "Credits Vendor",
+    ["zh-cn"] = "商店",
   },
   open_credits_vendor_view_key_description = {
     en = "Opens the Credits Vendor view.",
+    ["zh-cn"] = "打开军械交易所界面。",
   },
   -- inbox_view
   open_inbox_view_key = {
     en = "Inbox",
+    ["zh-cn"] = "信件",
   },
   open_inbox_view_key_description = {
     en = "Opens the Inbox view.",
+    ["zh-cn"] = "打开信件界面。",
   },
   -- mission_board_view
   open_mission_board_view_key = {
     en = "Mission Board",
+    ["zh-cn"] = "任务面板",
   },
   open_mission_board_view_key_description = {
     en = "Opens the Mission Board view.",
+    ["zh-cn"] = "打开任务面板。",
   },
   -- store_view
   open_premium_store_view_key = {
     en = "Premium Store",
+    ["zh-cn"] = "高级商店",
   },
   open_premium_store_view_key_description = {
     en = "Opens the Premium Store view.",
+    ["zh-cn"] = "打开准将的服装界面。",
   },
   -- training_grounds_view
   open_training_grounds_view_key = {
     en = "Training Grounds",
+    ["zh-cn"] = "训练场",
   },
   open_training_grounds_view_key_description = {
     en = "Opens the Training Grounds view.",
+    ["zh-cn"] = "打开灵能室界面。",
   },
 }
